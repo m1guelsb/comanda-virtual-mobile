b3e08f73b13ac7286c51b8db1e9edccb334dd559
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-22 22:52:38 -0300

    [feat] add Header component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,6 +1,9 @@
+import { Header } from '../components/Header';
 export const Main = () => {
   return (
     <Container>
+      <Header />
+
     </Container>
   );
 };
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { Text } from '../Text';
+import { Container } from './header.styles';
+
+export const Header = () => {
+  return (
+    <Container>
+      <Text size={14} opacity={0.9}>
+        Bem vindo(a)
+      </Text>
+      <Text size={24} weight={'700'}>
+        Comanda Virtual
+      </Text>
+    </Container>
+  );
+};
