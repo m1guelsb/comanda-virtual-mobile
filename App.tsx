1b32dc299ec905e0f328a33c18d0bab393c79d6e
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-22 22:51:19 -0300

    Initial Configs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { Main } from './src/Main';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'Poppins-400': require('./src/assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-500': require('./src/assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-700': require('./src/assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  if (!isFontsLoaded) return null;
+
+  const queryClient = new QueryClient();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <StatusBar style="dark" />
+        <Main />
+      </QueryClientProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Main/main.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import { Platform, StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Container = styled.SafeAreaView`
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  padding: 24px;
+  flex-grow: 1;
+  flex-direction: column;
+  gap: 24px;
+  background-color: #f1f1f1;
+`;
