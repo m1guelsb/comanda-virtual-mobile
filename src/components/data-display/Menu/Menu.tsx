079964950537f31f0fdaf2e0c5dc475180ae2535
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-27 16:36:06 -0300

    [feat] Menu - add useListProducts hook

--- src/components/data-display/Menu/Menu.tsx
+++ src/components/data-display/Menu/Menu.tsx
@@ -1,7 +1,11 @@
-import { FlatList, TouchableOpacity, View } from 'react-native';
-import { useQuery } from '@tanstack/react-query';
+import {
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SvgXml } from 'react-native-svg';
 import { API_URL } from '@env';
-import { api } from '@/services/axios';
 import { Text } from '@/components/typography';
 import { formatCurrency } from '@/utils/formatCurrency';
 import type { Product } from '@/types';
@@ -11,93 +15,98 @@ import {
   Image,
   Details,
 } from './menu.styles';
-import { SvgXml } from 'react-native-svg';
 import { add } from '@/assets/icons';
+import { useListProducts } from '@/hooks/api';
+import { useIsFetching } from '@tanstack/react-query';
 
 interface MenuProps {
   onAddToCart(product: Product): void;
 }
 export const Menu = ({ onAddToCart }: MenuProps) => {
-  const { data: productList } = useQuery({
-    queryKey: ['products'],
-    queryFn: async () =>
-      await api.get<Product[]>('/products').then((res) => res.data),
-    refetchOnWindowFocus: false,
-  });
+  const { productList } = useListProducts();
+  const isProductListFetching = useIsFetching({ queryKey: ['products'] });
 
   return (
     <>
-      <Container>
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          ItemSeparatorComponent={() => (
-            <View
-              style={{
-                height: 16,
-              }}
-            />
-          )}
-          data={productList}
-          contentContainerStyle={{ paddingVertical: 8 }}
-          keyExtractor={(product) => product.id}
-          renderItem={({ item: product }) => {
-            return (
-              <ProductItem>
-                <Image
-                  source={{
-                    uri: `${API_URL}/uploads/${product.imagePath}`,
-                    headers: {
-                      'Content-type': 'application/json',
-                    },
-                    method: 'GET',
-                  }}
-                />
-
-                <Details>
-                  <Text weight="700" numberOfLines={1}>
-                    {product.name}
-                  </Text>
-                  <Text
-                    numberOfLines={2}
-                    style={{ lineHeight: 15 }}
-                    size={14}
-                    color="#666"
-                  >
-                    {product.description}
-                  </Text>
-
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'space-between',
+      {isProductListFetching ? (
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <ActivityIndicator size={96} />
+        </View>
+      ) : (
+        <Container>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={() => (
+              <View
+                style={{
+                  height: 16,
+                }}
+              />
+            )}
+            data={productList}
+            contentContainerStyle={{ paddingVertical: 8 }}
+            keyExtractor={(product) => product.id}
+            renderItem={({ item: product }) => {
+              return (
+                <ProductItem>
+                  <Image
+                    source={{
+                      uri: `${API_URL}/uploads/${product.imagePath}`,
+                      headers: {
+                        'Content-type': 'application/json',
+                      },
+                      method: 'GET',
                     }}
-                  >
-                    <Text
-                      size={14}
-                      style={{ display: 'flex', alignItems: 'center' }}
-                      weight="700"
-                    >
-                      {formatCurrency(product.price)}
+                  />
+
+                  <Details>
+                    <Text weight="700" numberOfLines={1}>
+                      {product.name}
                     </Text>
-                  </View>
-                </Details>
-                <TouchableOpacity onPress={() => onAddToCart(product)}>
-                  <View
-                    style={{
-                      flex: 1,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <SvgXml xml={add} />
-                  </View>
-                </TouchableOpacity>
-              </ProductItem>
-            );
-          }}
-        />
-      </Container>
+                    <Text
+                      numberOfLines={2}
+                      style={{ lineHeight: 15 }}
+                      size={14}
+                      color="#666"
+                    >
+                      {product.description}
+                    </Text>
+
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <Text
+                        size={14}
+                        style={{ display: 'flex', alignItems: 'center' }}
+                        weight="700"
+                      >
+                        {formatCurrency(product.price)}
+                      </Text>
+                    </View>
+                  </Details>
+                  <TouchableOpacity onPress={() => onAddToCart(product)}>
+                    <View
+                      style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <SvgXml xml={add} />
+                    </View>
+                  </TouchableOpacity>
+                </ProductItem>
+              );
+            }}
+          />
+        </Container>
+      )}
     </>
   );
 };
