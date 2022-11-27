34754db235297763d2b83bf15d2290cb70898eee
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-27 17:42:46 -0300

    [feat] Menu - add loading and no content states

--- src/components/data-display/Menu/Menu.tsx
+++ src/components/data-display/Menu/Menu.tsx
@@ -1,9 +1,4 @@
-import {
-  ActivityIndicator,
-  FlatList,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { FlatList, TouchableOpacity, View } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import { API_URL } from '@env';
 import { Text } from '@/components/typography';
@@ -18,6 +13,7 @@ import {
 import { add } from '@/assets/icons';
 import { useListProducts } from '@/hooks/api';
 import { useIsFetching } from '@tanstack/react-query';
+import { MenuSkeleton, NoContent } from '@/components/feedback';
 
 interface MenuProps {
   onAddToCart(product: Product): void;
@@ -27,86 +23,82 @@ export const Menu = ({ onAddToCart }: MenuProps) => {
   const isProductListFetching = useIsFetching({ queryKey: ['products'] });
 
   return (
-    <>
+    <Container>
       {isProductListFetching ? (
-        <View
-          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        >
-          <ActivityIndicator size={96} />
-        </View>
-      ) : (
-        <Container>
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            ItemSeparatorComponent={() => (
-              <View
-                style={{
-                  height: 16,
-                }}
-              />
-            )}
-            data={productList}
-            contentContainerStyle={{ paddingVertical: 8 }}
-            keyExtractor={(product) => product.id}
-            renderItem={({ item: product }) => {
-              return (
-                <ProductItem>
-                  <Image
-                    source={{
-                      uri: `${API_URL}/uploads/${product.imagePath}`,
-                      headers: {
-                        'Content-type': 'application/json',
-                      },
-                      method: 'GET',
+        <MenuSkeleton />
+      ) : productList && productList?.length > 0 ? (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                height: 16,
+              }}
+            />
+          )}
+          data={productList}
+          contentContainerStyle={{ paddingVertical: 8 }}
+          keyExtractor={(product) => product.id}
+          renderItem={({ item: product }) => {
+            return (
+              <ProductItem>
+                <Image
+                  source={{
+                    uri: `${API_URL}/uploads/${product.imagePath}`,
+                    headers: {
+                      'Content-type': 'application/json',
+                    },
+                    method: 'GET',
+                  }}
+                />
+
+                <Details>
+                  <Text weight="700" numberOfLines={1}>
+                    {product.name}
+                  </Text>
+                  <Text
+                    numberOfLines={2}
+                    style={{ lineHeight: 15 }}
+                    size={14}
+                    color="#666"
+                  >
+                    {product.description}
+                  </Text>
+
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
                     }}
-                  />
-
-                  <Details>
-                    <Text weight="700" numberOfLines={1}>
-                      {product.name}
-                    </Text>
+                  >
                     <Text
-                      numberOfLines={2}
-                      style={{ lineHeight: 15 }}
                       size={14}
-                      color="#666"
+                      style={{ display: 'flex', alignItems: 'center' }}
+                      weight="700"
                     >
-                      {product.description}
+                      {formatCurrency(product.price)}
                     </Text>
-
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                      }}
-                    >
-                      <Text
-                        size={14}
-                        style={{ display: 'flex', alignItems: 'center' }}
-                        weight="700"
-                      >
-                        {formatCurrency(product.price)}
-                      </Text>
-                    </View>
-                  </Details>
-                  <TouchableOpacity onPress={() => onAddToCart(product)}>
-                    <View
-                      style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }}
-                    >
-                      <SvgXml xml={add} />
-                    </View>
-                  </TouchableOpacity>
-                </ProductItem>
-              );
-            }}
-          />
-        </Container>
+                  </View>
+                </Details>
+                <TouchableOpacity onPress={() => onAddToCart(product)}>
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <SvgXml xml={add} />
+                  </View>
+                </TouchableOpacity>
+              </ProductItem>
+            );
+          }}
+        />
+      ) : (
+        <NoContent message="Sem produtos no estoque" />
       )}
-    </>
+    </Container>
   );
 };
